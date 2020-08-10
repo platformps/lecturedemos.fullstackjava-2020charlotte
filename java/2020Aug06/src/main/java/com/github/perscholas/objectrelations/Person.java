ca34be27d5b66c51077c9adb55e489b02e2a32b8
git-leon <xleonhunter@gmail.com>
2020-08-10 09:56:03 -0400

    'update'

---
+++ java/2020Aug06/src/main/java/com/github/perscholas/objectrelations/Person.java
@@ -0,0 +1,7 @@
+package com.github.perscholas.objectrelations;
+
+/**
+ * Created by leon on 8/6/2020.
+ */
+public class Person {
+}
---
+++ java/2020Aug10/src/main/java/com/github/perscholas/IOEx1.java
@@ -0,0 +1,37 @@
+package com.github.perscholas;
+
+import java.io.*;
+
+/**
+ * Created by leon on 8/10/2020.
+ */
+public class IOEx1 {
+    public static void main(String[] args) throws IOException {
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+        String projectLocalDirectory = System.getProperty("user.dir");
+        String fileInPath = projectLocalDirectory + "/src/main/resources/input.txt";
+        String fileOutPath = projectLocalDirectory + "/src/main/resources/output.txt";
+
+        File readIn = new File(fileInPath);
+        File writeTo = new File(fileOutPath);
+
+        try {
+            inputStream = new FileReader(readIn);
+            outputStream = new FileWriter(writeTo);
+            int c;
+            do {
+                c = inputStream.read();
+                outputStream.write(c);
+            } while(c != -1);
+        } finally {
+            if(inputStream != null) {
+                inputStream.close();
+            }
+            if(outputStream != null) {
+                outputStream.close();
+            }
+        }
+
+    }
+}
