7bd7588e6fa2d7e1fb7c024863880ddaee7ecdba
git-leon <xleonhunter@gmail.com>
2020-09-01 15:07:33 -0400

    'update'

---
+++ java/2020Sept01/lecture2/src/main/java/com/github/perscholas/User.java
@@ -0,0 +1,39 @@
+package com.github.perscholas;
+
+import javax.persistence.*;
+
+/**
+ * Created by leon on 9/1/2020.
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String username;
+    String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
